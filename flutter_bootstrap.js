fc16fbdd9ee31e06a126a6e982289dfbdd47b78c
h2techjun <h2techjun@gmail.com>
2026-03-29 09:10:35 +0900

    fix: card overlay i18n - pass AppStrings to all HwatuCard instances

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1636112698" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1669020853" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
